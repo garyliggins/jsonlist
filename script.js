b6e3712f8aa5fd4ed209d5661d490529df66b2a9
gary <gary@ligginsdevelopment.com>
2021-07-14 15:05:10 -0700

    project almost complete

--- script.js
+++ script.js
@@ -3,24 +3,55 @@
 const output = document.querySelector('.output');
 console.log(output);
 const url = 'list.json'
+let myList = [];
+let localData = localStorage.getItem('myList')
+console.log(localData)
 
 window.addEventListener('DOMContentLoaded', () => {
-    output.textContent = 'loading..';
+    // output.textContent = 'loading..';
+    if(localData){
+        myList = JSON.parse(localStorage.getItem('myList'))
+        maker();
+    } else {
     fetch(url).then(rep => rep.json())
     .then((data) => {
-        console.log(data);
-        output.innerHTML = "";
-        data.forEach(element => {
-            makeList(element)
-        });
+        myList = data;
+        // console.log(data);
+       maker()
+        localStorage.setItem('myList',JSON.stringify(myList))
     })
+}
+    
 })
 
-function makeList(element) {
+function maker() {
+    myList.forEach((element,index) => {
+        makeList(element,index)
+    });
+}
+
+function makeList(element,index) {
     const div = document.createElement('div')
     div.innerHTML = `${element.name} Number of Guests: ${element.guests}`;
     output.append(div)
+    if(element.status){
+    div.classList.add('active');}
+    else  { element.status = false 
+        div.classList.add('notActive');
+        }
+    div.addEventListener('click', (e)=> {
 
-    div.classList.add('active');
-    
-}
+        div.classList.toggle('active');
+        div.classList.toggle('notActive')
+        console.log(div.classList.contains('active'));
+        if(div.classList.contains('active')){
+            myList[index].status = true }
+            else {
+                myList[index].status = false
+            }
+            localStorage.setItem('myList',JSON.stringify(myList))
+        console.log(myList)
+    })
+
+
+    }
