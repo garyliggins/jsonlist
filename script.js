54c5278fb02277d03c2b2eb2ddd6f5a2824a68c8
gary <gary@ligginsdevelopment.com>
2021-07-14 17:50:34 -0700

    almost finished with list

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 
 
 const output = document.querySelector('.output');
-console.log(output);
+const btn1 = document.createElement('button');
+btn1.textContent = "Reload JSON Data"
+
+btn1.addEventListener('click', reloader)
+
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+const btn2 = document.createElement('button');
+
+const div1 = document.createElement('div')
+div1.append(input1);
+div1.append(input2);
+div1.append(btn2);
+btn2.textContent = 'Add to List';
+input1.setAttribute('placeholder', 'Name')
+input2.setAttribute('type', 'number')
+input2.value = '1'
+document.body.append(div1)
+document.body.append(btn1)
+btn2.addEventListener('click', addToList);
+
+
+
+document.body.append(btn1);
+
+
+
 const url = 'list.json'
 let myList = [];
 let localData = localStorage.getItem('myList')
@@ -18,11 +44,21 @@ window.addEventListener('DOMContentLoaded', () => {
         myList = data;
         // console.log(data);
        maker()
-        localStorage.setItem('myList',JSON.stringify(myList))
+       saveToStorage();
     })
 }
     
 })
+
+function reloader(){
+    fetch(url).then(rep => rep.json())
+    .then((data) => {
+        myList = data;
+        // console.log(data);
+       maker()
+       saveToStorage();
+    })
+}
 
 function maker() {
     myList.forEach((element,index) => {
@@ -32,8 +68,12 @@ function maker() {
 
 function makeList(element,index) {
     const div = document.createElement('div')
+    div.classList.add('box')
     div.innerHTML = `${element.name} Number of Guests: ${element.guests}`;
     output.append(div)
+
+    
+
     if(element.status){
     div.classList.add('active');}
     else  { element.status = false 
@@ -49,9 +89,39 @@ function makeList(element,index) {
             else {
                 myList[index].status = false
             }
-            localStorage.setItem('myList',JSON.stringify(myList))
-        console.log(myList)
+            saveToStorage()
+    })
+    const span = document.createElement('span');
+    span.textContent = 'X';
+    div.append(span)
+    span.addEventListener('click',(e) => {
+        console.log(e)
+            console.log(index)
+        e.stopPropagation();
+        div.remove();
+        myList.splice(index,1)
+       saveToStorage();
     })
 
 
+    }
+
+    function saveToStorage(){
+        console.log(myList)
+        localStorage.setItem('myList',JSON.stringify(myList))
+    }
+
+    function addToList() {
+        console.log(input1.value);
+        console.log(input2.value);
+        const myObj ={
+            'name': input1.value,
+            'guests': input2.value,
+            'status': false
+
+        }
+        const val = myList.length;
+        myList.push(myObj);
+        saveToStorage();
+        makeList(myObj,val)
     }
